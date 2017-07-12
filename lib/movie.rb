6fc6dad215dbfb66d2061e512431ea0eb7a40927
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-12 17:33:16 +0300

    debugged one of the most difficult things ever in my history

--- lib/movie.rb
+++ lib/movie.rb
@@ -41,6 +41,18 @@ class Movie
     end
   end
 
+  define_method(:actors) do
+  movie_actors = []
+  results = DB.exec("SELECT actor_id FROM actors_movies WHERE movie_id = #{self.id()};")
+  results.each() do |result|
+    actor_id = result.fetch("actor_id").to_i()
+    actor = DB.exec("SELECT * FROM actors WHERE id = #{actor_id};")
+    name = actor.first().fetch("name")
+    movie_actors.push(Actor.new({:name => name, :id => actor_id}))
+  end
+  movie_actors
+end
+
     define_method(:delete) do
       DB.exec("DELETE FROM movies WHERE id = #{self.id()};")
     end
